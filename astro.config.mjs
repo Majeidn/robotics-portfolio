66fed1c81a0b43900a6406931f21a75e22392008
Mark Kheyfets <147564858+Majeidn@users.noreply.github.com>
2026-06-20 15:21:27 -0400

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://majeidn.github.io',
+    base: '/robotics-portfolio',
+});
